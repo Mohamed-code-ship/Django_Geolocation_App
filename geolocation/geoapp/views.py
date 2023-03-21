395c3c245793672a21dcb1ce95786a4bc804fb71
Mohacode <mohaabdul568@gmail.com>
2023-03-21 20:50:29 +0300

    First Commit

--- geolocation/geoapp/views.py
+++ geolocation/geoapp/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
+import requests
+import json
 # Create your views here.
 
 def index(request):
-    return render (request, 'index.html')
+    res = requests.get('http://ip-api.com/json/24.48.0.1')
+    location_data_one = res.text
+    location_data = json.loads(location_data_one)
+
+    return render (request,'index.html'{'data':location_data})
+
+
+
